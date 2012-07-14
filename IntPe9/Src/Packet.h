907a1178bdffd03a9deeaeafa619702942d70270
Intline9 <Intline9@gmail.com>
2012-07-15 00:54:38 +0200

    Licence linked in all files
    I was talking to a frend about licences and i forgot that i should
    update each and every file with licence. Please do honor my licence even
    for previous revisions!

--- IntPe9/Src/Packet.h
+++ IntPe9/Src/Packet.h
@@ -1,19 +1,20 @@
-//IntPe9 an general all purpose packet editor
-//Copyright (C) 2012  Intline9 <Intline9@gmail.com>
-//
-//This program is free software: you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation, either version 3 of the License, or
-//(at your option) any later version.
+/*
+IntPe9 a open source multi game, general, all purpose and educational packet editor
+Copyright (C) 2012  Intline9 <Intline9@gmail.com>
 
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-//
-//You should have received a copy of the GNU General Public License
-//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef PACKET_H
 #define PACKET_H
 
--- IntPe9/Views/FilterView.h
+++ IntPe9/Views/FilterView.h
@@ -1,3 +1,20 @@
+/*
+IntPe9 a open source multi game, general, all purpose and educational packet editor
+Copyright (C) 2012  Intline9 <Intline9@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef FILTER_VIEW_H
 #define FILTER_VIEW_H
 
